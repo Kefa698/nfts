d9165901b89cba065b1d342647957319070944d1
Kefa698 <kefaombongi07@gmail.com>
2022-11-02 18:43:39 +0300

    bsforeEach

---
+++ test/nfts.test.js
@@ -0,0 +1,20 @@
+const { assert, expect } = require("chai")
+const { network, deployments, ethers } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Nft Marketplace Unit Tests", function () {
+          let nftMarketplace, nftMarketplaceContract, basicNft, basicNftContract
+
+          beforeEach(async () => {
+              accounts = await ethers.getSigners()
+              deployer = accounts[0]
+              user = accounts[1]
+              await deployments.fixture(["all"])
+              nftMarketplaceContract = await ethers.getContract("Nft")
+              nftMarketplace = await nftMarketplaceContract.connect(deployer)
+              basicNftContract = await ethers.getContract("BasicNft")
+              basicNft = await basicNftContract.connect(deployer)
+          })
+      })

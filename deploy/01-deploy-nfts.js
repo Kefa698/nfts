cd71e8673244a29070907c7034a212e0abede6f9
Kefa698 <kefaombongi07@gmail.com>
2022-11-02 18:28:24 +0300

    fixed

--- deploy/01-deploy-nfts.js
+++ deploy/01-deploy-nfts.js
@@ -26,4 +26,4 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     log("----------------------------------------------------")
 }
 
-module.exports.tags = ["all", "nftmarketplace"]
+module.exports.tags = ["all", "Nft"]
